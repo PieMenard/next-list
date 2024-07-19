2ccc102c2d97083116191fcb8e478433cf4cef16
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-07-19 15:42:43 -0400

    add edit list dialog

--- app/components/ListCard.tsx
+++ app/components/ListCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -7,11 +9,27 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { List } from '@prisma/client';
+import { useState } from 'react';
 
 const ListCard = ({ list }: { list: List }) => {
+  const [openListDialog, setOpenListDialog] = useState(false);
+  const [editData, setEditData] = useState({
+    title: list.title,
+    description: list.description,
+  });
+
   return (
     <div>
       <Card className="w-[300px] h-auto bg-gray-950 text-white">
@@ -19,11 +37,52 @@ const ListCard = ({ list }: { list: List }) => {
           <CardTitle>{list.title}</CardTitle>
           <CardDescription>{list.description}</CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
-        <CardFooter className="flex gap-2">
-          <Button variant="secondary">Edit</Button>
+        <CardContent className="flex gap-2">
+          {/*Edit Button*/}
+          <Button variant="secondary" onClick={() => setOpenListDialog(true)}>
+            Edit
+          </Button>
+          {/*Dialog*/}
+          <Dialog open={openListDialog}>
+            <DialogContent className="sm:max-w-[425px] bg-gray-900">
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="title" className="text-right">
+                    Title
+                  </Label>
+                  <Input
+                    onChange={(e) =>
+                      setEditData({ ...editData, title: e.target.value })
+                    }
+                    value={editData.title}
+                    id="title"
+                    className="col-span-3 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="description" className="text-right">
+                    Description
+                  </Label>
+                  <textarea
+                    onChange={(e) =>
+                      setEditData({ ...editData, description: e.target.value })
+                    }
+                    value={editData.description}
+                    id="description"
+                    className="col-span-3 h-24 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit" onClick={() => handleEdit()}>
+                  Save changes
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          {/*Delete Button*/}
           <Button variant="destructive">Delete</Button>
-        </CardFooter>
+        </CardContent>
       </Card>
     </div>
   );
